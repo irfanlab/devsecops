99f15544653631f36e3e78e5460d11662f213e93
irfanlab <n.mohammedirfan88@gmail.com>
2026-03-27 19:15:16 +0530

    Update install-script.sh

--- setup/vm-install-script/install-script.sh
+++ setup/vm-install-script/install-script.sh
@@ -78,23 +78,18 @@ java -version
 mvn -v
 
 echo ".........----------------#################._.-.-JENKINS-.-._.#################----------------........."
-echo ".........----------------#################._.-.-JENKINS-.-._.#################----------------........."
 
 # Remove old broken config (important)
 rm -f /etc/apt/sources.list.d/jenkins.list
 rm -f /usr/share/keyrings/jenkins.gpg
 
-# Add Jenkins key properly
-curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key \
-  | tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
-
-# Add repo with signed-by
-echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/" \
-  | tee /etc/apt/sources.list.d/jenkins.list > /dev/null
-
-# Update and install
-apt-get update -y
-apt-get install -y jenkins
+sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2026.key
+echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt update
+sudo apt install jenkins
 
 # Start Jenkins
 systemctl daemon-reload
