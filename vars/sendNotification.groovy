b586f253fd94af51c4e4ed5290aef21885fbc7ba
Mohammed Irfan Nazeer <VT150@AD.vthink.local>
2026-04-10 18:14:02 +0530

    updated slack

--- vars/sendNotification.groovy
+++ vars/sendNotification.groovy
@@ -14,9 +14,9 @@ def call(String buildStatus = 'STARTED') {
   // emoji = ':hulk:'
  }
 
-// def msg = "${buildStatus}: `${env.JOB_NAME}` #${env.BUILD_NUMBER}:\n${env.BUILD_URL}"
+def msg = "${buildStatus}: `${env.JOB_NAME}` #${env.BUILD_NUMBER}:\n${env.BUILD_URL}"
 
-// slackSend(color: color, message: msg)
+slackSend(color: color, message: msg)
 
 //  attachments = [
 //     [
